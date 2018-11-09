53ca296457faad8457c5e388fab3808c3407b578
Chris Bojemski <chris.bojemski@gmail.com>
2018-11-09 15:14:09 -0500

    Fix CORS problem.

--- config/application.rb
+++ config/application.rb
@@ -32,8 +32,12 @@ module MonsterBoardGame
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => 'http://localhost'
-    }
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
   end
 end
